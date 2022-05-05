3c9e7d064064f00ad15d384c227b8fbc6f03ae63
Beagle <sarah.e.beagle@gmail.com>
2022-05-04 17:53:58 -0700

    Created fetch for Repos

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 //Document Selectors
 //Overview Div selection
 const overview = document.querySelector(".overview");
+const repoList = document.querySelector(".repo-list");
 
 //github username
 const username = "Beagles-Designs";
@@ -27,5 +28,23 @@ const displayUser = function(userData) {
             <p><strong>Number of public repos:</strong> ${userData.public_repos}</p>
          </div> `;
          overview.append(div);
+    getRepos();     
 
+};
+
+const getRepos = async function () {
+    const response = await fetch (`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repos = await response.json();
+    console.log(repos);
+    displayRepos(repos);
+};
+
+
+const displayRepos = function(repos){
+    for (const repo of repos) {
+        const li = document.createElement("li");
+        li.classList.add("repo");
+        li.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(li);
+    }
 };
